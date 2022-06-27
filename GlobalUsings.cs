39852fdb788458fa6abadbffafcfe224a743aad4
Gordon Moore <gmgithub@gordonmoore.co.uk>
2022-06-27 23:00:21 +0100

    Better test with Monkey type

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,4 +2,7 @@
 //global using Microsoft.Toolkit.Mvvm.ComponentModel;
 global using TestMVVMHelper.ViewModel;
 global using System.Windows.Input;
-
+global using CommunityToolkit.Mvvm.Input;
+global using System.Collections.ObjectModel;
+global using System.Net.Http;
+global using System.Net.Http.Json;
--- ViewModel/MainPageViewModel.cs
+++ ViewModel/MainPageViewModel.cs
@@ -1,43 +1,70 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using System.Collections.ObjectModel;
-
-namespace TestMVVMHelper.ViewModel;
+﻿namespace TestMVVMHelper.ViewModel;
 
 public partial class MainPageViewModel : ObservableObject
 {
   [ObservableProperty]
-  ObservableCollection<String> monkeys = new()
-  {
-    "Chimp",
-    "Gorilla",
-    "Orangutan",
-    "Gibbon"
-  };
+  ObservableCollection<Monkey> monkeys = new();
 
   [ObservableProperty]
-  string selectedItem;
+  private string greet = "Hello, Gordon!";
+
+  public MainPageViewModel()
+  {
+    GetData();
+  }
+
+  public async void GetData()
+  {
+    HttpClient httpClient = new();
+    var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+    if (response.IsSuccessStatusCode)
+    {
+      List<Monkey> l = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+      Monkeys = new ObservableCollection<Monkey>(l);
+    }
+  }
+
+  [ObservableProperty]
+  Monkey selectedItem;
 
   [RelayCommand]
   public async void SelectionChanged()
   {
     Console.WriteLine($"Hi {SelectedItem}");
 
-    //await Task.Delay(2000);
-
-    //SelectedItem = null;
-  }
-
-  [ObservableProperty]
-  private string greet = "Hello, Gordon!";
-
-  [RelayCommand]
-  public async void HitMe()
-  {
-    Console.WriteLine($"Hello");
-    Greet = "Hi, friend";
+    await Task.Delay(5000);
 
     SelectedItem = null;
   }
 
 
+
+  [RelayCommand]
+  public void HitMe()
+  {
+    Console.WriteLine($"Hello {SelectedItem}");
+    Greet = "Hi, friend";
+
+    SelectedItem = null;
+  }
+
 }
+
+public class Monkey
+{
+  public string Name { get; set; }
+  public string Location { get; set; }
+  public string Details { get; set; }
+  public string Image { get; set; }
+  public int Population { get; set; }
+  public double Latitude { get; set; }
+  public double Longitude { get; set; }
+}
+
+//ObservableCollection<String> monkeys = new()
+//{
+//  "Chimp",
+//  "Gorilla",
+//  "Orangutan",
+//  "Gibbon"
+//};
